78a212afdb7b6739d3869303e0c9ae82fd64b410
Jonathan Herbault <jonathanherbault@gmail.com>
2019-01-25 15:41:21 +0100

    Updates

---
+++ precache-manifest.3187dca56421efeb856e7a19dea46586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc67ec5f622ee934cb9",
+    "url": "/Player360/static/js/runtime~main.dcc67ec5.js"
+  },
+  {
+    "revision": "7f0ab59cd7cde8b9b2c0",
+    "url": "/Player360/static/js/main.7f0ab59c.chunk.js"
+  },
+  {
+    "revision": "7b238c2d5332284d532d",
+    "url": "/Player360/static/js/1.7b238c2d.chunk.js"
+  },
+  {
+    "revision": "7f0ab59cd7cde8b9b2c0",
+    "url": "/Player360/static/css/main.1115d17d.chunk.css"
+  },
+  {
+    "revision": "7b238c2d5332284d532d",
+    "url": "/Player360/static/css/1.fdff819c.chunk.css"
+  },
+  {
+    "revision": "f7b761209b14eca38ca9e46fbb79a711",
+    "url": "/Player360/index.html"
+  }
+];
